7558d272d0476a612ce7433cb340ec8b0bee08a1
ashishjuris <ashish.juris@gmail.com>
2025-03-20 18:41:17 +0530

    home section completed and job seekers section half compeleted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import Hero from '../Componets/Hero/Hero'
-import Maincontent from '../Componets/Maincontent/Maincontent'
-import Main from '../Componets/Main/Main'
-import ContactUs from '../Componets/ContactUs/ContactUs'
+import React from "react";
+import Hero from "../Componets/Hero/Hero";
+import Maincontent from "../Componets/Maincontent/Maincontent";
+import Main from "../Componets/Main/Main";
+import ContactUs from "../Componets/ContactUs/ContactUs";
+import SubscribeSection from "../Componets/subscribe-section/SubscribeSection";
 
 const Home = () => {
   return (
-<>
+    <>
+      <Hero />
+      <Maincontent />
+      <Main />
+      <ContactUs />
+      <SubscribeSection />
+      {/* <Sitemaintenance/> */}
+    </>
+  );
+};
 
-<Hero/>
-     <Maincontent/>
-     <Main/>
-     <ContactUs/>
-     {/* <Sitemaintenance/> */}
-</>  )
-}
-
-export default Home
+export default Home;
