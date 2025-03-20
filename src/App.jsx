68a1cbb905547f67d044804fce4a44b7af6ed782
sumit0079-tech <sumit.jurisnexa03@gmail.com>
2025-03-20 18:57:27 +0530

    vvv

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Componets/Navbar/Navbar";
 
-import Footer from "./Componets/Footer/footer";
+
 import AppRoutes from "./router/Approute";
+import Footer from "./Componets/Footer/footer";
 function App() {
   return (
     <>
