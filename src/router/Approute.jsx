9d934933c9d221e9e1e09bb74f5b8cea759f9f93
sumit0079-tech <sumit.jurisnexa03@gmail.com>
2025-03-21 15:58:45 +0530

    modify

--- src/router/Approute.jsx
+++ src/router/Approute.jsx
@@ -5,14 +5,15 @@ import JobSeekers from "../pages/JobSeekers";
  
 import ContactUsPage from "../pages/ContactUsPage";
 import AboutUs from "../Componets/AboutUs/AboutUs";
+import Employers from "../pages/Employers";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/about-us" element={<AboutUs />} />
-      <Route path="/job-seekers" element={<JobSeekers />} />
-      <Route path="/contact" element={<ContactUsPage />} />
+      <Route path="about-us" element={<AboutUs />} />
+      <Route path="job-seekers" element={<JobSeekers />} />
+      <Route path="employers" element={<Employers />} />
     </Routes>
   );
 };
