324f0cc31ce7247a24922019df7e682ce8c42677
ashishjuris <ashish.juris@gmail.com>
2025-03-21 14:50:07 +0530

    modification

--- src/router/Approute.jsx
+++ src/router/Approute.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
-
 import JobSeekers from "../pages/JobSeekers";
+ 
+import ContactUsPage from "../pages/ContactUsPage";
 import AboutUs from "../Componets/AboutUs/AboutUs";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="about-us" element={<AboutUs />} />
-      <Route path="job-seekers" element={<JobSeekers />} />
+      <Route path="/about-us" element={<AboutUs />} />
+      <Route path="/job-seekers" element={<JobSeekers />} />
+      <Route path="/contact" element={<ContactUsPage />} />
     </Routes>
   );
 };
