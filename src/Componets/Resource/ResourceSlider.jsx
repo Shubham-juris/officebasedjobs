4e88ed47ff0f171c0830c2e44b034551176c874e
rishavjuris <rishav.juris@gmail.com>
2025-03-21 15:59:38 +0530

    resource section

---
+++ src/Componets/Resource/ResourceSlider.jsx
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+import { Box, Typography, IconButton } from "@mui/material";
+import { ArrowBack, ArrowForward } from "@mui/icons-material";
+import SocialImage from "../../assets/Image/jobs.png";
+import MarketingImage from "../../assets/Image/officejobs.png";
+import SupportImage from "../../assets/Image/jobs.png";
+import CRMImage from "../../assets//Image/officejobs.png";
+
+
+const ResourceSlider = () => {
+    const images = [SocialImage, MarketingImage, SupportImage, CRMImage];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+      const interval = setInterval(() => {
+        handleNext();
+      }, 3000); 
+      return () => clearInterval(interval);
+    }, [currentIndex]);
+  
+    const handleNext = () => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+  
+    const handlePrev = () => {
+      setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    };
+  
+   
+    const getVisibleImages = () => {
+      return [
+        images[(currentIndex - 1 + images.length) % images.length], 
+        images[currentIndex % images.length], 
+        images[(currentIndex + 1) % images.length], 
+      ];
+    };
+  return ( 
+    <>
+      <div className='text-[2em] text-center mt-[6em]'>
+        <h1><b>Behind the Sences wiith office Based Jobs</b></h1>
+      </div> 
+      
+       <Box sx={{ textAlign: "center", bgcolor: "#fff", p: 4, position: "relative" }}>
+
+      {/* Image Container */}
+      <Box sx={{ 
+        display: "flex", 
+        alignItems: "center", 
+        justifyContent: "center", 
+        position: "relative",
+        overflow: "hidden", 
+        width: "100%"
+      }}>
+        {/* Previous Button */}
+        <IconButton 
+          onClick={handlePrev} 
+          sx={{ position: "absolute", left: 10, zIndex: 2 }}
+        >
+          <ArrowBack fontSize="large" />
+        </IconButton>
+
+        {/* Images */}
+        <Box sx={{ 
+          width: "80%", 
+          display: "flex", 
+          justifyContent: "center", 
+          alignItems: "center",
+          gap: 2 
+        }}>
+          {getVisibleImages().map((img, index) => (
+            <Box 
+              key={index} 
+              sx={{
+                width: index === 1 ? "80%" : "30%", 
+                opacity: index === 1 ? 1 : 0.6,
+                transform: index === 1 ? "scale(1.1)" : "scale(0.9)",
+                transition: "all 0.5s ease-in-out",
+              }}
+            >
+              <img 
+                src={img} 
+                alt="Portfolio" 
+                style={{ 
+                  width: "100%", 
+                  height: "350px", 
+                  objectFit: "contain", 
+                  
+                }} 
+              />
+            </Box>
+          ))}
+        </Box>
+
+        {/* Next Button */}
+        <IconButton 
+          onClick={handleNext} 
+          sx={{ position: "absolute", right: 10, zIndex: 2 }}
+        >
+          <ArrowForward fontSize="large" />
+        </IconButton>
+      </Box>
+    </Box>
+    </>
+  )
+}
+
+export default ResourceSlider
--- src/pages/Resource.jsx
+++ src/pages/Resource.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ResourceHeader from '../Componets/Resource/ResourceHeader';
+import ResourceTraning from '../Componets/Resource/ResourceTraning';
+import ResourceSection from '../Componets/Resource/ResourceSection';
+import ResourceSlider from '../Componets/Resource/ResourceSlider';
+import ResourceContact from '../Componets/Resource/ResourceContact';
 
 
 const Resource = () => {
   return (
     <>
    < ResourceHeader />
+   <ResourceTraning />
+   <ResourceSection />
+   <ResourceSlider />
+   <ResourceContact />
     </>
   )
 }
