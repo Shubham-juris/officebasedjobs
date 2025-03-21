37a4e91a063857ad4cb84a04b0d9cf68e143317b
sumit0079-tech <sumit.jurisnexa03@gmail.com>
2025-03-21 19:31:46 +0530

    helo

--- src/router/Approute.jsx
+++ src/router/Approute.jsx
@@ -7,7 +7,6 @@ import ContactUsPage from "../pages/ContactUsPage";
 import AboutUs from "../Componets/AboutUs/AboutUs";
 import Employers from "../pages/Employers";
 import Resource from "../pages/Resource";
-import Careers from "../pages/careers-xyz";
 
 const AppRoutes = () => {
   return (
@@ -17,9 +16,7 @@ const AppRoutes = () => {
       <Route path="job-seekers" element={<JobSeekers />} />
       <Route path="contact-us" element={<ContactUsPage />} />
       <Route path="employers" element={<Employers />} />
-      <Route path="contact-us" element={<ContactUsPage />} />
       <Route path="resource" element={<Resource />} />
-      <Route path="careers" element={<Careers />} />
     </Routes>
   );
 };
